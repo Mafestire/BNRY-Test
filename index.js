59c509a8a8cf86974d2580dc420621ce5b408cea
Mafestire <seabaraven@gmail.com>
2023-04-24 16:17:33 +0200

    updated

--- index.js
+++ index.js
@@ -27,13 +27,12 @@ app.use(express.json(), express.urlencoded({extended: true}));
 
 
 // --ENDPOINTS--
-// const newsApi = require('newsapi');
-// const apiNews = new newsApi('28760a25597345a9b25f2e94b0d9851d');
+const newsApi = require('newsapi');
+const apiNews = new newsApi('28760a25597345a9b25f2e94b0d9851d');
 
 app.get('/', (req, res) => {
     const newsApi = require('newsapi');
-    const apiNews = require('./.env')
-    // const apiNews = new newsApi('28760a25597345a9b25f2e94b0d9851d');
+    const apiNews = new newsApi('28760a25597345a9b25f2e94b0d9851d');
    
     apiNews.v2.everything({
         q: 'coding',
@@ -48,41 +47,6 @@ app.get('/', (req, res) => {
         res.send(response)
     });
 })
-// app.get('/', (req, res) => {
-//     const newsApi = require('newsapi');
-//     const apiNews = new newsApi('28760a25597345a9b25f2e94b0d9851d');
-   
-//     apiNews.v2.everything({
-//         q: 'coding',
-//         sources: 'engadget',
-//         domains: ' techcrunch.com',
-//         from: '2023-03-25',
-//         to: '2017-12-12',
-//         language: 'en',
-//         sortBy: 'relevancy',
-//     }).then(response => {
-//         console.log(response);
-//         res.send(response)
-//     });
-// })
-
-// app.get('/sources', (req, res) => {
-//     const newsApi = require('newsapi');
-//     const apiNews = new newsApi('28760a25597345a9b25f2e94b0d9851d');
-//     apiNews.v2.everything({
-//         q: 'technology',
-//         sources: "bbc.co.uk,the-verge",
-//         domains: 'bbc.co.uk',
-//         from: '2023-03-25',
-//         to: '2023-04-21',
-//         language: 'en',
-//         sortBy: 'date',
-//         page: 3
-//     }).then(response => {
-//         console.log(response);
-//         res.send(response)
-//     });
-// })
 
 // ---listening port---
 app.listen(port, () => {
